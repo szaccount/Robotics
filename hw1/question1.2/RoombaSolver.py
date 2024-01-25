97e677cb24b17c2b914ad9cb044a384d49a7859b
sean <sean@ubuntu-22.04.02-LTS.myguest.virtualbox.org>
2024-01-25 21:45:10 +0200

    Save

--- hw1/question1.2/RoombaSolver.py
+++ hw1/question1.2/RoombaSolver.py
@@ -13,16 +13,14 @@ from sklearn.neighbors import NearestNeighbors
 
 
 class RoombaSolver(Solver):
-    def __init__(self, sample_points, samples_for_edge, num_neighbors):
+    def __init__(self, sample_points, num_neighbors):
         """
         Param sample_points: represents general number of uniform samples on the map.
-        Param samples_for_edge: number of samples we take to verify edge is valid to move on.
         Param num_neighbors: max number of neighbors trying to connect each point in the roadmap.
         """
         super().__init__()
         # Initializing properties of solver
         self.sample_points = sample_points
-        self.samples_for_edge = samples_for_edge
         self.num_neighbors = num_neighbors
 
         self._collision_detection = {}
@@ -35,23 +33,6 @@ class RoombaSolver(Solver):
         x = random.uniform(self._x_min, self._x_max)
         y = random.uniform(self._y_min, self._y_max)
         return Point_2(FT(x), FT(y))
-
-    # Was originaly, DELETE
-    # def _create_random_point(self, robots):
-    #     point = self._sample_random_point()
-
-    #     # Set a point that for all robots it won't collide with an obstacle
-    #     is_valid_point = False
-    #     while not is_valid_point:
-    #         point = self._sample_random_point()
-    #         is_valid_point = all(
-    #             [
-    #                 self._collision_detection[robot].is_point_valid(point)
-    #                 for robot in robots
-    #             ]
-    #         )
-
-    #     return point
 
     def _connect_node_to_nodes_if_possible(
         self, new_node, nodes, indxs_to_connect, robot, roadmap
@@ -91,51 +72,19 @@ class RoombaSolver(Solver):
         neigh = NearestNeighbors(n_neighbors=self.num_neighbors + 1, algorithm="brute")
         neigh.fit(points_list)
 
+        # Finding neighbors and connecting to them
         for curr_index in range(len(nodes_list)):
             curr_point = points_list[curr_index]
             # can do with distance, then gives pair of two lists where second as here and first is of distances
             indx_neighbors_of_point = neigh.kneighbors(
                 [curr_point], return_distance=False
             )
-            # DELETE !!!!!!!!!!!
-            # print(
-            #     f"for point {curr_index=} the neighbors indexes are {indx_neighbors_of_point=}"
-            # )
             indx_neighbors_of_point = indx_neighbors_of_point.tolist()[0]
             indx_neighbors_of_point.remove(curr_index)
             curr_node = nodes_list[curr_index]
-            # !!!! extracted to function
-            # for neigh_index in indx_neighbors_of_point:
-            #     neigh_node = nodes_list[neigh_index]
-            #     # Check if can connect the two nodes
-            #     can_connect = self._collision_detection[robot].is_edge_valid(
-            #         Segment_2(curr_node, neigh_node)
-            #     )
-            #     if can_connect:
-            #         roadmap.add_edge(curr_node, neigh_node, weight=1)
             self._connect_node_to_nodes_if_possible(
                 curr_node, nodes_list, indx_neighbors_of_point, robot, roadmap
             )
-
-        # # Add random connections
-        # for _ in range(10):
-        #     v, u = random.sample(list(roadmap.nodes), 2)
-        #     print(f"points {u=} {v=}")
-        #     roadmap.add_edge(v, u, weight=1)
-        #
-        # for robot in robots:
-        #     # Add starting point of robot to the graph
-        #     roadmap.add_node(robot.start)
-        #     # Connect start to a random point
-        #     roadmap.add_edge(
-        #         robot.start, *random.sample(list(roadmap.nodes), 1), weight=1
-        #     )
-        #     # Add ending point of robot to the graph
-        #     roadmap.add_node(robot.end)
-        #     # Connect to end to a random point
-        #     roadmap.add_edge(
-        #         robot.end, *random.sample(list(roadmap.nodes), 1), weight=1
-        #     )
 
         return roadmap
 
@@ -145,9 +94,6 @@ class RoombaSolver(Solver):
             self.scene
         )
 
-        # Build collision detection for each robot
-        # !!!!!! DELETE
-        assert len(self.scene.robots) == 1, "Should only be single robot"
         robot = self.scene.robots[0]
         self._collision_detection[robot] = ObjectCollisionDetection(
             scene.obstacles, robot
@@ -163,35 +109,13 @@ class RoombaSolver(Solver):
 
         path_collection = (
             PathCollection()
-        )  # Initialize PathCollection (stores the path for each robot)
-        # for i, robot in enumerate(self.scene.robots):
-        #     self.log(f"Robot {i}")
-
-        #     # Check if there is a possible path for the robot in the graph
-        #     if not nx.algorithms.has_path(self._roadmap, robot.start, robot.end):
-        #         self.log(f"No path found for robot {i}")
-        #         return PathCollection()
-
-        #     # Get the shortest path for the robot
-        #     found_path = nx.algorithms.shortest_path(
-        #         self._roadmap, robot.start, robot.end
-        #     )
-        #     points = [
-        #         PathPoint(point) for point in found_path
-        #     ]  # Convert all points to PathPoints (to make a path out of them)
-        #     path = Path(points)  # Make a path from all PathPoints
-        #     path_collection.add_robot_path(
-        #         robot, path
-        #     )  # Add the current path for the current robot to the path collection
+        )  # Initialize PathCollection (stores the path for robot)
 
         # Check if there is a possible path for the robot in the graph
-        print("HERE !!!!")
         if not nx.algorithms.has_path(self._roadmap, robot.start, robot.end):
             self.log(f"No path found for robot")
-            print("no path !!!!")
             return PathCollection()
         # Get the shortest path for the robot
-        print("After if !!!!")
         found_path = nx.algorithms.shortest_path(self._roadmap, robot.start, robot.end)
         points = [
             PathPoint(point) for point in found_path
@@ -199,9 +123,9 @@ class RoombaSolver(Solver):
         path = Path(points)  # Make a path from all PathPoints
         path_collection.add_robot_path(
             robot, path
-        )  # Add the current path for the current robot to the path collection
+        )  # Add the current path for the robot to the path collection
 
-        self.log("Successfully found a path for all robots")
+        self.log("Successfully found a path for the robot")
         return path_collection
 
     @staticmethod
@@ -210,11 +134,6 @@ class RoombaSolver(Solver):
         # in format of: 'property_name': (Description, Default value, Type)
         return {
             "sample_points": ("Number of points to sample on map", 2000, int),
-            "samples_for_edge": (
-                "Number of sample along desired edge to verify it is valid",
-                50,
-                int,
-            ),
             "num_neighbors": (
                 "Maximum number of neighbors of point in the roadmap",
                 15,
--- hw1/question1.2/roomba_in_cafe.py
+++ hw1/question1.2/roomba_in_cafe.py
@@ -10,7 +10,7 @@ with open("coffee_shop.json", "r") as fp:
     scene = Scene.from_dict(json.load(fp))
 
 # "Solve" the scene (find paths for the robots)
-solver = RoombaSolver(sample_points=5000, samples_for_edge=10, num_neighbors=15)
+solver = RoombaSolver(sample_points=2000, num_neighbors=15)
 solver.load_scene(scene)
 path_collection = solver.solve()  # Returns a PathCollection object
 
@@ -26,20 +26,4 @@ for i, (robot, path) in enumerate(path_collection.paths.items()):
 result, reason = verify_paths(scene, path_collection)
 print(f"Are paths valid: {result}\t{reason}")
 
-# Optional - Open solver_viewer with our solver and scene
-
-# Option 1 - Use solve object we made:
-print("First gui")
 start_gui(scene, solver)
-
-# # Option 2 - Use solver's class type to create a new one:
-# print("Second gui")
-# start_gui(scene, RoombaSolver)
-
-# # Option 3 - Use solver's class name to create a new one (must pass solver's module):
-# print("Third gui")
-# start_gui(scene, "RoombaSolver", "RoombaSolver.py")
-
-# # Option 4 - Passing the path to the scene json file
-# print("Fourth gui")
-# start_gui("coffee_shop.json", solver)
