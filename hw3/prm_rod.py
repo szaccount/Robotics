4c59d96f75b88dca861b04f4f36b78495141a290
sean <sean@ubuntu-22.04.02-LTS.myguest.virtualbox.org>
2024-02-29 17:33:18 +0200

    Fixed paths and formatting

--- hw3/prm_rod.py
+++ hw3/prm_rod.py
@@ -4,16 +4,20 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from .. import Robot, RobotDisc, RobotPolygon, RobotRod
-from .. import Obstacle, ObstacleDisc, ObstaclePolygon, Scene
-from .. import PathPoint, Path, PathCollection
+from discopygal.solvers import Robot, RobotDisc, RobotPolygon, RobotRod
+from discopygal.solvers import Obstacle, ObstacleDisc, ObstaclePolygon, Scene
+from discopygal.solvers import PathPoint, Path, PathCollection
 
-from ..samplers import Sampler, Sampler_Uniform
-from ..metrics import Metric, Metric_Euclidean
-from ..nearest_neighbors import NearestNeighbors, NearestNeighbors_sklearn
-from ...bindings import *
-from ...geometry_utils import collision_detection, conversions
-from ..Solver import Solver
+from discopygal.solvers.samplers import Sampler, Sampler_Uniform
+from discopygal.solvers.metrics import Metric, Metric_Euclidean
+from discopygal.solvers.nearest_neighbors import (
+    NearestNeighbors,
+    NearestNeighbors_sklearn,
+)
+from discopygal.bindings import *
+from discopygal.geometry_utils import collision_detection, conversions
+from discopygal.solvers.Solver import Solver
+
 
 class BasicRodPRM(Solver):
     """
@@ -34,20 +38,29 @@ class BasicRodPRM(Solver):
     :param sampler: sampling algorithm/method. if None then use uniform sampling
     :type sampler: :class:`~discopygal.solvers.samplers.Sampler`
     """
-    def __init__(self, num_landmarks, k, bounding_margin_width_factor=Solver.DEFAULT_BOUNDS_MARGIN_FACTOR, nearest_neighbors=None, metric=None, sampler=None):
+
+    def __init__(
+        self,
+        num_landmarks,
+        k,
+        bounding_margin_width_factor=Solver.DEFAULT_BOUNDS_MARGIN_FACTOR,
+        nearest_neighbors=None,
+        metric=None,
+        sampler=None,
+    ):
         super().__init__(bounding_margin_width_factor)
         self.num_landmarks = num_landmarks
         self.k = k
 
-        self.nearest_neighbors : NearestNeighbors = nearest_neighbors
+        self.nearest_neighbors: NearestNeighbors = nearest_neighbors
         if self.nearest_neighbors is None:
             self.nearest_neighbors = NearestNeighbors_sklearn()
 
-        self.metric : Metric = metric
+        self.metric: Metric = metric
         if self.metric is None:
             self.metric = Metric_Euclidean
 
-        self.sampler : Sampler = sampler
+        self.sampler: Sampler = sampler
         if self.sampler is None:
             self.sampler = Sampler_Uniform()
 
@@ -67,9 +80,13 @@ class BasicRodPRM(Solver):
         :rtype: :class:`dict`
         """
         return {
-            'num_landmarks': ('Number of Landmarks:',1000, int),
-            'k': ('K for nearest neighbors:', 15, int),
-            'bounding_margin_width_factor': ('Margin width factor (for bounding box):', 0, FT),
+            "num_landmarks": ("Number of Landmarks:", 1000, int),
+            "k": ("K for nearest neighbors:", 15, int),
+            "bounding_margin_width_factor": (
+                "Margin width factor (for bounding box):",
+                0,
+                FT,
+            ),
         }
 
     @staticmethod
@@ -81,8 +98,14 @@ class BasicRodPRM(Solver):
         :param d: arguments dict
         :type d: :class:`dict`
         """
-        return BasicRodPRM(d['num_landmarks'], d['k'], FT(d['bounding_margin_width_factor']), None, None, None)
-
+        return BasicRodPRM(
+            d["num_landmarks"],
+            d["k"],
+            FT(d["bounding_margin_width_factor"]),
+            None,
+            None,
+            None,
+        )
 
     def get_graph(self):
         """
@@ -101,9 +124,11 @@ class BasicRodPRM(Solver):
         # Check validity of each edge seperately
         for robot in self.scene.robots:
             xy_edge = Segment_2(p[0], q[0])
-            if not self.collision_detection[robot].is_edge_valid((xy_edge, FT(p[1]), FT(q[1])), clockwise):
+            if not self.collision_detection[robot].is_edge_valid(
+                (xy_edge, FT(p[1]), FT(q[1])), clockwise
+            ):
                 return False
-            break # Only one robot is supported
+            break  # Only one robot is supported
         return True
 
     def sample_free(self):
@@ -115,13 +140,15 @@ class BasicRodPRM(Solver):
         while not self.collision_detection[robot].is_point_valid(sample):
             sample = (self.sampler.sample(), FT(random.random() * 2 * math.pi))
         return sample
-    
+
     def point2vec3(self, point):
         """
         Convert a point (xy, theta) to a 3D vector
         """
-        return Point_d(3, [point[0].x().to_double(), point[0].y().to_double(), point[1].to_double()])
-
+        return Point_d(
+            3,
+            [point[0].x().to_double(), point[0].y().to_double(), point[1].to_double()],
+        )
 
     def load_scene(self, scene: Scene):
         """
@@ -136,7 +163,9 @@ class BasicRodPRM(Solver):
 
         # Build collision detection for each robot
         for robot in scene.robots:
-            self.collision_detection[robot] = collision_detection.ObjectCollisionDetection(scene.obstacles, robot)
+            self.collision_detection[robot] = (
+                collision_detection.ObjectCollisionDetection(scene.obstacles, robot)
+            )
 
         ################
         # Build the PRM
@@ -154,24 +183,35 @@ class BasicRodPRM(Solver):
             p_rand = self.sample_free()
             self.roadmap.add_node(p_rand)
             if i % 100 == 0 and self.verbose:
-                print('added', i, 'landmarks in PRM', file=self.writer)
+                print("added", i, "landmarks in PRM", file=self.writer)
 
         self.nearest_neighbors.fit(list(map(self.point2vec3, self.roadmap.nodes)))
 
         # Connect all points to their k nearest neighbors
         last_nodes = list(self.roadmap.nodes)
         for cnt, point in enumerate(last_nodes):
-            neighbors = self.nearest_neighbors.k_nearest(self.point2vec3(point), self.k+1)
+            neighbors = self.nearest_neighbors.k_nearest(
+                self.point2vec3(point), self.k + 1
+            )
             for neighbor in neighbors:
                 neighbor = (Point_2(neighbor[0], neighbor[1]), neighbor[2])
                 for clockwise in [True, False]:
                     if self.collision_free(point, neighbor, clockwise):
                         weight = self.metric.dist(point[0], neighbor[0]).to_double()
-                        self.roadmap.add_edge(point, neighbor, weight=weight, clockwise=clockwise)
-                        self.roadmap.add_edge(neighbor, point, weight=weight, clockwise=(not clockwise))
+                        self.roadmap.add_edge(
+                            point, neighbor, weight=weight, clockwise=clockwise
+                        )
+                        self.roadmap.add_edge(
+                            neighbor, point, weight=weight, clockwise=(not clockwise)
+                        )
 
             if cnt % 100 == 0 and self.verbose:
-                print('connected', cnt, 'landmarks to their nearest neighbors', file=self.writer)
+                print(
+                    "connected",
+                    cnt,
+                    "landmarks to their nearest neighbors",
+                    file=self.writer,
+                )
 
     def solve(self):
         """
@@ -183,27 +223,30 @@ class BasicRodPRM(Solver):
         """
         if not nx.algorithms.has_path(self.roadmap, self.start, self.end):
             if self.verbose:
-                print('no path found...', file=self.writer)
+                print("no path found...", file=self.writer)
             return PathCollection()
 
         # Convert from a sequence of Point_d points to PathCollection
-        tensor_path = nx.algorithms.shortest_path(self.roadmap, self.start, self.end, weight='weight')
+        tensor_path = nx.algorithms.shortest_path(
+            self.roadmap, self.start, self.end, weight="weight"
+        )
         path_collection = PathCollection()
         for _, robot in enumerate(self.scene.robots):
             points = []
             for i, point in enumerate(tensor_path):
                 if point != self.end:
-                    clockwise = self.roadmap.get_edge_data(tensor_path[i], tensor_path[i+1])["clockwise"]
-                points.append(PathPoint(point[0], data = {'theta': point[1], 'clockwise':clockwise}))
+                    clockwise = self.roadmap.get_edge_data(
+                        tensor_path[i], tensor_path[i + 1]
+                    )["clockwise"]
+                points.append(
+                    PathPoint(
+                        point[0], data={"theta": point[1], "clockwise": clockwise}
+                    )
+                )
             path = Path(points)
             path_collection.add_robot_path(robot, path)
 
         if self.verbose:
-            print('successfully found a path...', file=self.writer)
+            print("successfully found a path...", file=self.writer)
 
         return path_collection
-
-
-
-
-
