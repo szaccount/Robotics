4bdef5ef0be2ab7d39d2045f0178a10c9c3e2fb8
sean <sean@ubuntu-22.04.02-LTS.myguest.virtualbox.org>
2024-03-09 18:50:26 +0200

    Save changes to experiment script

--- hw3/experiment.py
+++ hw3/experiment.py
@@ -10,14 +10,16 @@ from prm_rod import BasicRodPRM
 from prm_rod_improved import ImprovedRodPRM
 
 
-scene_json = "rod_scenes/1_rod_example.json"
+scene_json = "rod_scenes/2_rod_example.json"
 
 with open(scene_json, "r") as fp:
     scene = Scene.from_dict(json.load(fp))
 
-NUM_EXPERIMENTS = 2
-NUM_LANDMARKS = 400
+NUM_EXPERIMENTS = 10
+NUM_LANDMARKS = 500
 K = 15
+
+print(f"{scene_json=}, {NUM_EXPERIMENTS=}, {NUM_LANDMARKS=}, {K=}")
 
 success_counter = 0
 start = timer()
@@ -32,7 +34,7 @@ for i in range(NUM_EXPERIMENTS):
     # ]
 
     result, reason = verify_paths(scene, path_collection)
-    print(f"Are paths valid: {result}\t{reason}")
+    # print(f"Are paths valid: {result}\t{reason}")
     if result:
         success_counter += 1
 
@@ -45,30 +47,36 @@ print("For the basic PRM:")
 print(
     f"Did {NUM_EXPERIMENTS} experiments, average_time={average_time}, success_rate={success_rate}%"
 )
+print()
 
-success_counter_improved = 0
-start_improved = timer()
-for i in range(NUM_EXPERIMENTS):
-    # "Solve" the scene (find paths for the robots)
-    solver = ImprovedRodPRM(num_landmarks=NUM_LANDMARKS, k=K, gaussian_ratio=0.9)
-    solver.load_scene(scene)
-    path_collection = solver.solve()  # Returns a PathCollection object
 
-    # solution = [
-    #     (robot, path) for i, (robot, path) in enumerate(path_collection.paths.items())
-    # ]
+for gaus_ratio in [0.2, 0.8, 1]:
+    success_counter_improved = 0
+    start_improved = timer()
+    for i in range(NUM_EXPERIMENTS):
+        # "Solve" the scene (find paths for the robots)
+        solver = ImprovedRodPRM(
+            num_landmarks=NUM_LANDMARKS, k=K, gaussian_ratio=gaus_ratio
+        )
+        solver.load_scene(scene)
+        path_collection = solver.solve()  # Returns a PathCollection object
 
-    result, reason = verify_paths(scene, path_collection)
-    print(f"Improved Are paths valid: {result}\t{reason}")
-    if result:
-        success_counter_improved += 1
+        # solution = [
+        #     (robot, path) for i, (robot, path) in enumerate(path_collection.paths.items())
+        # ]
 
-    # start_gui(scene, solver)
-end_improved = timer()
+        result, reason = verify_paths(scene, path_collection)
+        # print(f"Improved Are paths valid: {result}\t{reason}")
+        if result:
+            success_counter_improved += 1
 
-average_time_improved = (end_improved - start_improved) / NUM_EXPERIMENTS
-success_rate_improved = (success_counter_improved / NUM_EXPERIMENTS) * 100
-print("For the improved PRM:")
-print(
-    f"Did {NUM_EXPERIMENTS} experiments, average_time={average_time_improved}, success_rate={success_rate_improved}%"
-)
+        # start_gui(scene, solver)
+    end_improved = timer()
+
+    average_time_improved = (end_improved - start_improved) / NUM_EXPERIMENTS
+    success_rate_improved = (success_counter_improved / NUM_EXPERIMENTS) * 100
+    print(f"For the improved PRM with {gaus_ratio=}:")
+    print(
+        f"Did {NUM_EXPERIMENTS} experiments, average_time={average_time_improved}, success_rate={success_rate_improved}%"
+    )
+    print()
